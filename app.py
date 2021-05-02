a74c07d4e21fc9c19b8ff6339b0841f1964d3f20
kfpro <njrfarhandasilva10@gmail.com>
2021-05-02 17:06:31 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 """
 python app.py
 """
+
+
+from flask import Flask, render_template, request, url_for, send_from_directory, jsonify, send_file
+import os
 from vaccine_availability import getSlots, convert_to_str, send_mail
 
+
+# "templates" this is for plain html files or "Great_Templates" this is for complex css + imgs +js +html+sass
+TEMPLATES = "templates"
+
+app = Flask(__name__, static_folder="assets", template_folder=TEMPLATES)
+ROOT_DIR = os.getcwd()
+
+# Reloading
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+"""
 if __name__ == '__main__':
     flag_available, Available_Slots = getSlots(DIST_ID=255, numdays=20, age=21)
     msg = "No available slots found"
@@ -13,3 +36,4 @@ if __name__ == '__main__':
     for mail in MAILS:
         send_mail(body, receiver_email=mail,
                   subject='VACCINE AVAILABILITY NOTIFICATION')
+"""
